b0b319e0f0b61ed95ad70358095ab3283e9faad3
JaeHoon Lee <ljhgood9925@gmail.com>
2020-11-26 22:33:03 +0900

    객체지향 function, method 기초

--- BasicTheory2.py
+++ BasicTheory2.py
@@ -6,7 +6,11 @@ class People:
     legs = 2
     foot = 2
 
-
+    # 이건 method
+    def setEyes(self, eyes_):
+        self.eyes = eyes_
+        
+# 이건 function
 def init():
     jaeHoon = People()
     print("재훈이 눈 개수 :", jaeHoon.eyes)
@@ -15,6 +19,12 @@ def init():
 
     jaeHoon.heads = 1
     print("재훈이 머리 개수 :", jaeHoon.heads)
+    print()
+    jaeHoon.setEyes(3)
+    print("재훈이 눈 개수 :", jaeHoon.eyes)
 
+
+
+# 똑같은 함수인데 클래스 안에 있는건 메서드라 말한다.
 
 init()
